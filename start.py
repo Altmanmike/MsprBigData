9b0e228ffd37dd7f0772337e83da676f0cfc7927
Mael.Legouge <mael892001@gmail.com>
2023-07-05 16:13:28 +0200

    ahout des fichier python

--- start.py
+++ start.py
@@ -1,5 +1,9 @@
 # Début de programme en Python pour la MSPR BIG DATA - traitement des données, modèle d'entraînement, etc...
 import os
-
+import mike
+import mael
+import ibti
+import fatima
+import yannis
 # Importation des données au format .csv
 # Traiement en 5 parties
