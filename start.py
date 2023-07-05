761e1143afbebf27e7684f4ad408f0d9e4ed03ab
Michael ALTMAN <altman_mike@yahoo.fr>
2023-07-05 16:04:17 +0200

    first commit

---
+++ start.py
@@ -0,0 +1,5 @@
+# Début de programme en Python pour la MSPR BIG DATA - traitement des données, modèle d'entraînement, etc...
+import os
+
+# Importation des données au format .csv
+# Traiement en 5 parties
