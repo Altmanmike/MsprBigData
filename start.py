26ccbc09ef67aac1e4eecaf29f600e951aa1b319
Michaël Altman <67266613+Altmanmike@users.noreply.github.com>
2023-08-22 13:50:12 +0200

    Delete start.py

--- start.py
+++
@@ -1,9 +0,0 @@
-# Début de programme en Python pour la MSPR BIG DATA - traitement des données, modèle d'entraînement, etc...
-import os
-import mike
-import mael
-import ibti
-import fatima
-import yannis
-# Importation des données au format .csv
-# Traiement en 5 parties
